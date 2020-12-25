35e453b5b67624ee6cafcf1aae1be6abefc84428
alenagorcakova <wernite1812@gmail.com>
2020-12-25 15:13:58 +0300

    Initial Commit

---
+++ testGit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  testGit
+//
+//  Created by Алена Горчакова on 25.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
